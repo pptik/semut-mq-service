92cf3e526a85acb34e8a4e94c6ef13e152a18201
Hendra Permana <hynra@yahoo.com>
2017-01-11 11:58:10 +0700

    Add db conn

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-var send = require('./amqp/tests/test_service');
+var mysql = require('mysql');
+var appconfig = require('./setup/configs.json');
+
+//connect to db
+var connection = mysql.createConnection({
+    host     : appconfig.mysql['hostname'],
+    user     : appconfig.mysql['user'],
+    password : appconfig.mysql['password'],
+    database : appconfig.mysql['database']
+});
+
+
+function connectToMysql(callback){
+    connection.connect(function (err) {
+        if(err){
+            callback({status: false, msg: "MySql : Connection to  "+appconfig.mysql['hostname'] + " error : "+err});
+        }else {
+            callback({status: true, msg: "MySql :  Connection to  "+appconfig.mysql['hostname'] + " success"});
+        }
+    });
+}
+
+connectToMysql(function(result){
+    if(result.status == true){
+        exports.conn = connection;
+        console.log(result.msg);
+    }else {
+        console.log(result.msg);
+    }
+});
+
+
+//var send = require('./amqp/tests/test_service');
