f59bb3ecab7d232f9d3fa817c8120901d96cba39
Hendra Permana <hynra@yahoo.com>
2017-02-16 23:08:08 +0700

    store location & mapview routing

--- amqp/users.js
+++ amqp/users.js
@@ -7,11 +7,24 @@ exports.updateLocation = function (msg) {
     console.log(req);
     rest.post('http://localhost:3030/api/location/store', {
         headers: {'Content-Type':'application/x-www-form-urlencoded'},
-        data: req
+        data: req['store']
     }).on('complete', function(data, response) {
-        app.chnannel.sendToQueue(msg.properties.replyTo, new Buffer(JSON.stringify(data)), {
-            correlationId: msg.properties.correlationId,
-            type: appconfig.type.callback
-        });
+        var success = (data['success'] == true) ? true : false;
+        if(success) {
+            rest.post('http://167.205.7.226:3030/api/location/mapview', {
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                data: req['mapview']
+            }).on('complete', function (data, response) {
+                app.chnannel.sendToQueue(msg.properties.replyTo, new Buffer(JSON.stringify(data)), {
+                    correlationId: msg.properties.correlationId,
+                    type: appconfig.type.callback
+                });
+            });
+        }else {
+            app.chnannel.sendToQueue(msg.properties.replyTo, new Buffer(JSON.stringify(data)), {
+                correlationId: msg.properties.correlationId,
+                type: appconfig.type.callback
+            });
+        }
     });
 };
