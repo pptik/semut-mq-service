b85db41f0711b3e348a23c5692fc18b2bd367ef8
Hendra Permana <hynra@yahoo.com>
2017-02-16 23:11:20 +0700

    edit config

--- amqp/users.js
+++ amqp/users.js
@@ -5,13 +5,13 @@ var rest = require('restler');
 exports.updateLocation = function (msg) {
     var req = JSON.parse(msg.content.toString());
     console.log(req);
-    rest.post('http://localhost:3030/api/location/store', {
+    rest.post(appconfig.rest_api_uri+'location/store', {
         headers: {'Content-Type':'application/x-www-form-urlencoded'},
         data: req['store']
     }).on('complete', function(data, response) {
         var success = (data['success'] == true) ? true : false;
         if(success) {
-            rest.post('http://167.205.7.226:3030/api/location/mapview', {
+            rest.post(appconfig.rest_api_uri+'location/mapview', {
                 headers: {'Content-Type': 'application/x-www-form-urlencoded'},
                 data: req['mapview']
             }).on('complete', function (data, response) {
