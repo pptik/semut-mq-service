75bb3626b8532a241fb00e76a8712c54b1b51006
Hendra Permana <hynra@yahoo.com>
2017-03-17 14:48:11 +0700

    update store location method

--- amqp/users.js
+++ amqp/users.js
@@ -7,19 +7,14 @@ exports.updateLocation = function (msg) {
     console.log(req);
     rest.post(appconfig.rest_api_uri+'location/store', {
         headers: {'Content-Type':'application/x-www-form-urlencoded'},
-        data: req['store']
+        data: req
     }).on('complete', function(data, response) {
-        var success = (data['success'] == true) ? true : false;
+        var success = (data['success'] == true);
         if(success) {
-            rest.post(appconfig.rest_api_uri+'location/mapview', {
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                data: req['mapview']
-            }).on('complete', function (data, response) {
-                app.chnannel.sendToQueue(msg.properties.replyTo, new Buffer(JSON.stringify(data)), {
-                    correlationId: msg.properties.correlationId,
-                  //  type: appconfig.type.callback
-                    type: msg.properties.type
-                });
+            app.chnannel.sendToQueue(msg.properties.replyTo, new Buffer(JSON.stringify(data)), {
+                correlationId: msg.properties.correlationId,
+                //  type: appconfig.type.callback
+                type: msg.properties.type
             });
         }else {
             app.chnannel.sendToQueue(msg.properties.replyTo, new Buffer(JSON.stringify(data)), {
